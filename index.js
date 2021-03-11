65aca454798680b07b529ad2f7835133abae4753
Michajuu <73675435+Michajuu@users.noreply.github.com>
2021-03-11 13:20:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Brawo otwarłeś drzwi";
 }
-function myFunction1() {
-  document.getElementById("demo1").innerHTML = "bruh";
+function bruh() {
+  document.getElementById("demo").innerHTML = "bruh";
 }
